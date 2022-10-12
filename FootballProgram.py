35437d31363cf17b63eb31c0a50f5ca18d46f14e
NickQuigley02 <nquigley@hawk.iit.edu>
2022-10-12 14:08:39 -0500

    Starts Yards to Integer process

--- FootballProgram.py
+++ FootballProgram.py
@@ -31,12 +31,40 @@ print('Field names are:' + ', '.join(field for field in fields))
 print('\nFirst 5 rows are:\n')
 for row in rows[:5]:
     # parsing each column of a row
+    print((int)(row[7].split(" ")[1])) ##Primarily testing right here, casting a split of this to an int to test out how to make location a workable int.
+    if((int)(row[7].split(" ")[1]) == 50):
+        print((int)(row[7].split(" ")[1]))
+    elif(row[1] == "Seahawks" and (row[7].split(" ")[0]) == "SEA"):
+        print((int)(row[7].split(" ")[1])) ##Representing a location closer to your endzone (0 + yardage)
+    elif (row[1] == "Seahawks" and (row[7].split(" ")[0]) != "SEA"):
+        print(100-(int)(row[7].split(" ")[1])) ##Representing a location closer to opp's endzone (100 - yardage)
+    elif(row[1] == "Packers" and (row[7].split(" ")[0]) == "GNB"):
+        print((int)(row[7].split(" ")[1])) ##Representing a location closer to your endzone (0 + yardage)
+    elif (row[1] == "Packers" and (row[7].split(" ")[0]) != "GNB"):
+        print(100-(int)(row[7].split(" ")[1])) ##Representing a location closer to opp's endzone (100 - yardage)
+    elif(row[1] == "Bills" and (row[7].split(" ")[0]) == "BUF"):
+        print((int)(row[7].split(" ")[1])) ##Representing a location closer to your endzone (0 + yardage)
+    elif (row[1] == "Bills" and (row[7].split(" ")[0]) != "BUF"):
+        print(100-(int)(row[7].split(" ")[1])) ##Representing a location closer to opp's endzone (100 - yardage)
+    elif(row[1] == "Cardinals" and (row[7].split(" ")[0]) == "CRD"):
+        print((int)(row[7].split(" ")[1])) ##Representing a location closer to your endzone (0 + yardage)
+    elif (row[1] == "Cardinals" and (row[7].split(" ")[0]) != "CRD"):
+        print(100-(int)(row[7].split(" ")[1])) ##Representing a location closer to opp's endzone (100 - yardage)
+    elif(row[1] == "Saints" and (row[7].split(" ")[0]) == "NOR"):
+        print((int)(row[7].split(" ")[1])) ##Representing a location closer to your endzone (0 + yardage)
+    elif (row[1] == "Saints" and (row[7].split(" ")[0]) != "NOR"):
+        print(100-(int)(row[7].split(" ")[1])) ##Representing a location closer to opp's endzone (100 - yardage)
+    
     for col in row:
         print("%10s"%col,end=" "),
+        
     print('\n')
 ## COURTESY OF GEEKS FOR GEEKS    
 for col in rows[5000]:
     print("%10s"%col,end=" "),
+print("\n\n\n")
+print(row[7].split(" "))
+    
 
 #For future reference
     #I have to find a way to convert something like "NEW 45" to an int based on whose playing the ball.
